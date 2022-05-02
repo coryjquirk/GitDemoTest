4b171e46a628bac2cf7aa9396c07e2dec0c748fa
Cory Quirk <coryjquirk@gmail.com>
2022-05-02 16:23:00 -0500

    println to print, removed returns

--- src/main/java/teksystems/java/MyClass.java
+++ src/main/java/teksystems/java/MyClass.java
@@ -1,61 +1,8 @@
 package teksystems.java;
 
-
-
-
-
-
-
-
-
-
-
-
-
 public class MyClass {
     public static void main(String[] args) {
-
-
-
-
-
-
-
-
-
-
-
-        System.out.println("Hello git branching");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.print("Hello git branching\n");
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
